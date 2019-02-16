a2331adac56e9e450d6d253ce9142855e7f9fc20
Piotr Wieliczko <p.wieliczko@gmail.com>
2019-02-16 10:25:53 +0100

    first commit

---
+++ src/main/java/pl/sda/addressbook/Main.java
@@ -0,0 +1,28 @@
+package pl.sda.addressbook;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+
+    public static void main(String[] args) {
+        // write your code here
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root =
+                FXMLLoader.load(
+                        getClass().getResource("/root.fxml"));
+        primaryStage.setScene(new Scene(root, 600, 350));
+        primaryStage.show();
+    }
+
+}
